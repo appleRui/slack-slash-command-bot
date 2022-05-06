38397a22fb6663bbef0d26ae5931a8d781c91a32
appleRui <apple31581@gmail.com>
2022-05-06 18:38:21 +0900

    feature/slash-commadn and modal-form

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv/config')
+const axios = require('axios')
 const express = require('express')
 const notion = require('./modules/notion.js')
 const todoist = require('./modules/todoist.js')
+const handOverModal = require('./views/handOverModal')
 
 const app = express()
 const port = 3000
@@ -72,6 +74,61 @@ app.post('/memo', async (req, res, next) => {
       }
     }]
   })
+})
+
+app.post('/modal', async (req, res, next) => {
+  const {
+    trigger_id
+  } = req.body
+  const url = 'https://slack.com/api/views.open'
+  const view = handOverModal
+  const header = {
+    headers: {
+      "Authorization": `Bearer ${process.env.SLACK_TOKEN}`,
+      "Content-type": 'application/json'
+    }
+  }
+  const params = {
+    trigger_id,
+    view,
+  }
+
+  try {
+    await axios.post(url, params, header)
+  } catch (e) {
+    console.error(e.request.status)
+  }
+  res.status(200).send(null)
+})
+
+app.post('/send', async (req, res, next) => {
+  const {
+    view,
+  } = JSON.parse(req.body.payload)
+
+  const ttl = view.state.values.ttl['plain_text_input-action'].value
+  const subject = view.state.values.subject['static_select-action'].selected_option.text.text
+  const detail = view.state.values.detail['plain_text_input-action'].value
+  const improvement = view.state.values.improvement['plain_text_input-action'].value
+
+  const url = 'https://hooks.slack.com/services/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'
+  const header = {
+    headers: {
+      "Content-type": 'application/json'
+    }
+  }
+  const text = `タイトル:${ttl}\n教材内容:${subject}\n詳細：${detail}\n改善方法:${improvement}`
+  const params = {
+    text
+  }
+
+  try {
+    const result = await axios.post(url, params, header)
+    console.log(result.statusText)
+  } catch (e) {
+    console.error(e.request)
+  }
+  res.status(200).send(null)
 })
 
 app.listen(port, () => {
